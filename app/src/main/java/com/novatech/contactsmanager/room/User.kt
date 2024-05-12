a9236a9226f6a1b00b59024cee38d2a19b825aa8
Novjean J Kannathara <novjeanj@gmail.com>
2024-05-12 12:44:46 +0530

    defined entity

---
+++ app/src/main/java/com/novatech/contactsmanager/room/User.kt
@@ -0,0 +1,17 @@
+package com.novatech.contactsmanager.room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user")
+data class User (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "user_id")
+    val id: Int,
+
+    @ColumnInfo(name = "user_name")
+    val name:String,
+
+    @ColumnInfo(name = "user_email")
+    val email:String)
